9c08af49dac98cb67de04fd16ac2299bd36591cf
Kovacs Áron László <kovacs.aron@arteries.hu>
2019-11-04 08:45:16 +0100

    finish stock project

--- 18.stocktrader/src/main.js
+++ 18.stocktrader/src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueReasource from 'vue-resource'
 
 import App from './App.vue'
 import { routes } from './routes'
 import store from './store/store';
 
 Vue.use(VueRouter);
+Vue.use(VueReasource);
+
+Vue.http.options.root = 'https://udemyvue-dafb0.firebaseio.com/';
 
 Vue.filter('currency', (value) => {
   return '$' + value.toLocaleString();
